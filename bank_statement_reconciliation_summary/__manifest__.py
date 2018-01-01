01371f00806d978e0a2d980159fbdf62969cf823
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2018-01-01 16:29:19 +0100

    Trig runbot build

--- bank_statement_reconciliation_summary/__manifest__.py
+++ bank_statement_reconciliation_summary/__manifest__.py
@@ -2,6 +2,7 @@
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Bank Statement Reconciliation Summary',
     'category': 'Account',
